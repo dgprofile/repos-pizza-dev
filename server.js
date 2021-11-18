885f546925740d8abae2c0d4ab286e206c9ecfab
debasish ghosh <profile.dghosh@gmail.com>
2021-11-17 22:35:33 -0500

    checkin for heroku

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';  
+import {APP_PORT,DB_URL} from './config';
+import router from './routes';
+import { createSecretKey } from 'crypto';
+import errorHandler from './middlewares/errorHandler';
+import mongoose from 'mongoose';
+import path from 'path';
+// Database connection
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useFindAndModify: false,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('DB connected...');
+});
+
+
+global.appRoot = path.resolve(__dirname);
+
+const app = express();
+
+app.use(express.json());
+app.use('/api',router);
+app.use('/uploads', express.static('uploads'));
+app.get('/',(req,res)=>res.send('hello'));
+
+
+app.use(errorHandler);
+app.listen(APP_PORT, ()=>{
+    console.log(`listening to port: ${APP_PORT}`)
+});
